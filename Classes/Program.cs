99b2d10e3f6f0c9b61ac65c182d7fe54375bf096
Ford Henley <info@fordswebhelp.com>
2025-09-24 20:42:54 -0400

    completed class exercise one

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Create instance of the Car class
+            
+            Car carOne = new Car();
+            carOne.Make = "Ford";
+            carOne.Model = "Fiesta";
+            carOne.Year = 2000;
+            
+            //Print the values of each property to the Console
+            
+            Console.WriteLine($"Make: {carOne.Make}, Model: {carOne.Model}, Year: {carOne.Year}");
         }
     }
 }
